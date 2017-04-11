df034cf3e2a27264eb6570fa8cb2277eb19eeda4
Aaron Levine <ald.levine@gmail.com>
2017-04-11 08:52:59 -0400

    Added further properties and functions for events

--- lib/event/include/event.h
+++ lib/event/include/event.h
@@ -1,9 +1,21 @@
+#ifndef MONKEYBAR_EVENT_H
+#define MONKEYBAR_EVENT_H
+
+#include <stdbool.h>
+
 #include "trie.h"
 
 typedef struct event_s {
   char        *type;
-  trie_node_t *data;
+  bool        cancellable;
+  bool        cancelled;
+  trie_node_t *data; /* Should hide trie implementation */
 } event_t;
 
 event_t* event_create (char *type, trie_node_t *data);
+
 void event_free (event_t *event);
+
+bool event_cancel (event_t *event);
+
+#endif /* MONKEYBAR_EVENT_H */
--- lib/event/include/event_target.h
+++ lib/event/include/event_target.h
@@ -1,13 +1,24 @@
+#ifndef MONKEYBAR_EVENT_TARGET_H
+#define MONKEYBAR_EVENT_TARGET_H
+
 #include "trie.h"
 #include "event.h"
 
 typedef struct event_target_s {
+  uint8_t     id;
   char        *type;
-  trie_node_t *properties;
+  trie_node_t *properties; /* Should hide trie implementation */
+  trie_node_t *events; /* Should hide trie implementation */
 } event_target_t;
 
-event_target_t *event_target_create (char *type, trie_node_t *properties);
+event_target_t* event_target_create (char *type, trie_node_t *properties);
+
 void event_target_free (event_target_t *event_target);
-bool event_target_register_event (char *type/*, ... */);
+
+bool event_target_insert_event (char *type/*, ... */);
+
 bool event_target_emit_event (event_t *event);
+
 bool event_target_handle_event (event_t *event);
+
+#endif /* MONKEYBAR_EVENT_TARGET_H */
