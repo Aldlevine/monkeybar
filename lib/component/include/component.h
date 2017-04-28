5290b29e7b5fd3001f65b9240a9eec5494070a95
Aaron Levine <ald.levine@gmail.com>
2017-04-27 20:21:48 -0400

    Finished simple parts of component

--- lib/component/include/component.h
+++ lib/component/include/component.h
@@ -15,7 +15,7 @@ typedef struct _Component_s {
  *
  * @return  the newly created component
  */
-Component * component_create (char *type);
+Component * component_create (char *type, Dictionary *properties);
 
 /**
  * frees an component
@@ -23,5 +23,10 @@ Component * component_create (char *type);
  * @param  component  the component to free
  */
 void component_free (Component *component);
+
+#define component_add_event(component, type, callback) \
+event_target_add_event(component->event_target, type, callback)
+
+void component_emit_event (Component *component, Event *event);
 
 #endif /* MONKEYBAR_COMPONENT_H */
--- lib/component/src/component.c
+++ lib/component/src/component.c
@@ -2,11 +2,12 @@
 #include "component.h"
 
 Component *
-component_create (char *type)
+component_create (char *type, Dictionary *properties)
 {
+  if (properties == NULL) properties = dictionary_create(NULL);
   Component *component = malloc(sizeof(Component));
   component->type = type;
-  component->properties = dictionary_create();
+  component->properties = properties;
   component->event_target = event_target_create();
   return component;
 }
@@ -16,4 +17,11 @@ component_free (Component *component)
 {
   event_target_free(component->event_target);
   free(component);
+}
+
+void
+component_emit_event (Component *component, Event *event)
+{
+  dictionary_set(event_data(event), "COMPONENT", component);
+  event_target_emit_event(component->event_target, event);
 }
